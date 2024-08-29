d73dc10e6e8139072504f09de0c9b5c42c9bd6ed
Felix Olivares <felixoe@gmail.com>
2024-08-29 14:02:19 -0700

    Remove references of "Items" in favor of "Images" which is a domain term used by domain experts in the specs.

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -12,5 +12,5 @@ public protocol FeedStore {
     typealias InsertCompletion = (Error?) -> Void
 
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertCompletion)
+    func insert(_ feed  : [LocalFeedImage], timestamp: Date, completion: @escaping InsertCompletion)
 }
