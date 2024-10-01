bf65b314b568bbe28e065388d3def3fc2204593f
Felix Olivares <felixoe@gmail.com>
2024-10-01 16:43:22 -0700

    Replace `FeedPresenter`from iOS module with the `FeedPresenter` from the cross-platform EssentialFeed module.

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by José Félix Olivares Estrada on 01/10/24.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
