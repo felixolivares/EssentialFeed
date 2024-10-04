4997bcd278e8e6e7557c9bd6207347e80f28abfe
Felix Olivares <felixoe@gmail.com>
2024-10-03 19:17:02 -0700

    Add empty EssentialApp project.

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by José Félix Olivares Estrada on 03/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
